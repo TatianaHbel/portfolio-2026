4c92bb74eb75d75e66e0821a33147ccd61db6373
thebell <tati.hbel@gmail.com>
2026-04-26 21:43:38 +0200

    Add Google Analytics integration and refactor font preloading in BaseLayout
    - Introduced Google Analytics tracking by adding the GA ID to the configuration and integrating the tracking script in BaseLayout.
    - Refactored font preloading to use a dynamic mapping approach for better maintainability and readability.

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,5 +1,7 @@
 export const resumeUrl = '/Resume/CV_Tatiana Hernandez_2026.pdf';
 
+export const gaId = 'G-VK8HBQF5MC';
+
 export const socialLinks = [
   { label: 'Linkedin', href: 'https://www.linkedin.com/in/tatianahernandezbel/' },
   { label: 'Github', href: 'https://github.com/TatianaHbel/' },
